3c6f94e92d48219416a6ac6d2b9122facd7a0dc8
S0ng-G <1617588572@qq.com>
2025-06-26 17:20:50 +0800

    Update main.py

--- v2.1/main.py
+++ v2.1/main.py
@@ -7,6 +7,11 @@ from MainLogic import LogicMixin
 class MainWindow(QMainWindow, LogicMixin):
     def __init__(self):
         super(MainWindow, self).__init__()
+    
+    def resizeEvent(self, event):
+        if hasattr(self, 'last_frame') and self.last_frame is not None:
+            self.display_image(self.last_frame)
+        event.accept()
 
     def closeEvent(self, event):
         self.stop_flag = True
